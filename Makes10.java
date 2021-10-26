0ca59c6d81d82a6f0f94148c3e30f07d37afa200
Rajeswarilakshmi <70252607+Rajeswarilakshmi@users.noreply.github.com>
2021-10-26 17:23:23 +0530

    makes10.java

---
+++ Makes10.java
@@ -0,0 +1,23 @@
+package com.bating;
+
+public class Makes10 {
+	public boolean makes10(int a, int b) {
+        if ((a==10|b==10)|(a+b==10)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Makes10 m=new Makes10();
+        System.out.println(m.makes10(9,10));
+        System.out.println(m.makes10(9,9));
+        System.out.println(m.makes10(1,9));
+
+	
+
+	}
+
+}
